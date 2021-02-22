2c4531cacd725c3a93c1897b404501137d4b6c21
Tomek Regulski <tomek.regulski@gmail.com>
2021-02-22 12:56:29 -0500

    added more styling and corrected functionality for end of game

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
 # quiz-app
+
+* [Description ](#description)
+* [What I Learned](#what-i-learned)
+* [Installation and Usage](#installation-and-usa)
+* [License](#license)
+
+![https://github.com/tomekregulski/portfolio/blob/main/assets/images/placeholder.png]
+
+# Description
+
+This is a simple, multiple-choice quiz app. Upon pressing "start", the user is present with a question and four answer choices. Choosing an incorrect answer will display a brief "wrong!" message, and subtract 4 seconds from the time remaining. Choosing a correct answer will display a brief "correct!" message, increase the score by 1, and display a new question. The game concludes once the timer reaches 0, or if the user manages to answer all the questions. At this point, the user's final score is displayed, and they are given the chance to submit their name. If the score is the new high score, then the user's name and score will be displayed as the high score.
+
+This page has been deployed on Github as a work-in-progress, and can be viewed here:  https://tomekregulski.github.io/quiz-app/
+
+# What I Learned
+
+This project has served to not only solidify what I have learned about HTML and CSS thus far, but also challenged me to expand my working knowledge of the languages in order to meet the functionality requirements.
+
+I am excited to know that I am now capable of producing an elegant site that is responsive to different screen sizes, makes use of makes use of pseudo classes to enhance certain parts of the user experience, and even make use of a few lines of JavaScript/jQuery to include a collapsible menu for smaller screens. Additionally, this project helped me gain confidence in turning to the online community for solutions to specific design and functionality issues.
+
+# Installation and Use
+
+Clone the repo and open the folder in the code editor of your choice. 
+
+# License
+
+MIT License
+
+Copyright (c) [2021] [Tomek Regulski]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,3 @@
-/* incorrect answer penalties are firing upon button creation */
-
 var timer;
 var timerCount;
 var score = 0;
@@ -12,9 +10,12 @@ var timerElement = document.querySelector("#timerCount");
 var questionNumber = 0;
 var body = document.querySelector("#body");
 var yourName = document.querySelector("#yourName");
+var correctText = document.querySelector("#correctText");
+var wrongText = document.querySelector("#wrongText");
 
 startButton.addEventListener("click", startGame);
-
+correctText.innerHTML = "&nbsp;&nbsp;&nbsp;Correct!&nbsp;&nbsp;&nbsp;";
+wrongText.innerHTML = "&nbsp;&nbsp;&nbsp;Wrong!&nbsp;&nbsp;&nbsp;";
 
 var questions = [
 {
@@ -77,58 +78,105 @@ var questions = [
     choices: ["no", "ain't", "not", "yes"],
     correct: "2",
 },
+{
+    prompt: "The answer is ain't",
+    choices: ["yes", "no", "not", "ain't"],
+    correct: "2",
+},
+{
+    prompt: "The answer is also ain't",
+    choices: ["no", "yes", "ain't", "not"],
+    correct: "2",
+},
+{
+    prompt: "The answer might be ain't",
+    choices: ["ain't", "not", "yes", "no"],
+    correct: "1",
+},
+{
+    prompt: "The answer is definitely ain't",
+    choices: ["no", "ain't", "not", "yes"],
+    correct: "2",
+},
 ];
 
 function askQuestion() {
+    correctText.setAttribute("id", "correctText");
+    wrongText.setAttribute("id", "wrongText");
     var question = questions[questionNumber];
     var ask = document.createElement('span');
+    ask.setAttribute("class", "mt-4");
     ask.textContent = questions[questionNumber].prompt;
     ask.id = "ask";
     questionPrompt.appendChild(ask);
     for (var i = 0; i < question.choices.length; i++) {
         var choice = document.createElement('button');
         choice.textContent = question.choices[i];
-        choice.id = i;
-        var id = choice.id;
-        choice.setAttribute("class", "btn-sm btn-primary px-4 mx-2")
+        choice.id = "button" + (i + 1);
+        choice.value = i;
+        choice.setAttribute("class", "btn-sm btn-primary px-4 mx-2");
         choicesDisplay.appendChild(choice);
     }
-    var btns = document.querySelectorAll('button');
-    for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', checkAnswer) 
-    }
+    var btn1 = document.querySelector("#button1");
+    var btn2 = document.querySelector("#button2");
+    var btn3 = document.querySelector("#button3");
+    var btn4 = document.querySelector("#button4");
+    btn1.addEventListener('click', checkAnswer);
+    btn2.addEventListener('click', checkAnswer);
+    btn3.addEventListener('click', checkAnswer);
+    btn4.addEventListener('click', checkAnswer);
 };
   
 function checkAnswer() {
+    var ask = document.querySelector("#ask");
     var question = questions[questionNumber];
-    if (this.id == question.correct) {
+    var btn1 = document.querySelector("#button1");
+    var btn2 = document.querySelector("#button2");
+    var btn3 = document.querySelector("#button3");
+    var btn4 = document.querySelector("#button4");
+    if (this.value == question.correct) {
+        correctText.setAttribute("id", "correctText.show");
         score++;
         console.log(this.id);
         console.log(score);
         questionNumber++;
         questionPrompt.removeChild(ask);
-        for ( var i = 0; i < question.choices.length; i++) {
-            var elem = document.getElementById(i);
-            elem.parentNode.removeChild(elem);
-        } 
-        askQuestion();
+        choicesDisplay.removeChild(btn1);
+        choicesDisplay.removeChild(btn2);
+        choicesDisplay.removeChild(btn3);
+        choicesDisplay.removeChild(btn4);
+        if (questionNumber < questions.length){
+            setTimeout(function() { askQuestion(); }, 500);
+        }
     } else {
-    // timerCount -= 10;
-    // body.style.opacity = "0.5";
-    // choices.style = "transform: translate(200px, 0); transition-duration: 5s";
+        wrongText.setAttribute("id", "wrongText.show");
+        timerCount -= 3;
+        setTimeout(function() { wrongText.setAttribute("id", "wrongText"); }, 500);
+        // body.style.opacity = "0.5";
+        // choices.style = "transform: translate(200px, 0); transition-duration: 5s";
     }
 };
 
 function startTimer() {
+  startButton.setAttribute("class", "d-none");
   // Sets timer
   timer = setInterval(function() {
     timerCount--;
     timerElement.textContent = timerCount;
     // Tests if time has run out
-    if (timerCount <= 0) {
-      questionPrompt.removeChild(ask);
-      while (choicesDisplay.firstChild) {
-        choicesDisplay.removeChild(choicesDisplay.lastChild);
+    if (timerCount <= 0 || questionNumber == questions.length) {
+      correctText.setAttribute("id", "correctText");
+      if (questionNumber < questions.length) {
+        var ask = document.querySelector("#ask");
+        var btn1 = document.querySelector("#button1");
+        var btn2 = document.querySelector("#button2");
+        var btn3 = document.querySelector("#button3");
+        var btn4 = document.querySelector("#button4");
+        questionPrompt.removeChild(ask);
+        choicesDisplay.removeChild(btn1);
+        choicesDisplay.removeChild(btn2);
+        choicesDisplay.removeChild(btn3);
+        choicesDisplay.removeChild(btn4);
       }
       clearInterval(timer);
       timerCount = 0;
@@ -157,6 +205,7 @@ function startGame() {
     var field = document.createElement('input');
     var submit = document.createElement('button');
     label.textContent = "Please enter your name: ";
+    label.setAttribute("class", "mr-2")
     form.id = "#form";
     label.for = "player";
     label.type = "text"
@@ -166,7 +215,7 @@ function startGame() {
     field.name = "player";
     submit.textContent = "Submit";
 
-    submit.setAttribute("class", "btn-sm btn-success px-4 mx-2");
+    submit.setAttribute("class", "btn-sm btn-success px-4 mt-2");
     field.setAttribute("class", "form-control");
     div.setAttribute("class", "form-inline");
     // add elements to DOM
@@ -174,7 +223,7 @@ function startGame() {
     form.appendChild(div);
     div.appendChild(label);
     div.appendChild(field);
-    form.appendChild(submit);
+    target.appendChild(submit);
     submit.addEventListener("click", submitNameLocal);
 };
 
@@ -192,12 +241,14 @@ function submitNameLocal(event) {
     play.id = "play"
     play.textContent = "Play Again?"
     target.appendChild(play);
+    play.setAttribute("class", "btn btn-primary px-4 mt-5");
     play.addEventListener("click", playAgain);
 };
 
 function playAgain() {
     var form = document.getElementById('#form');
     startButton.disabled = false;
+    startButton.setAttribute("class", "btn btn-primary px-4 mt-5");
     yourName.removeChild(play);
     questionNumber = 0;
     timerCount = 0;
