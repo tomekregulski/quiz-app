5de9c9503c77f626c7b7d6e28553231c4e1cf553
Tomek Regulski <tomek.regulski@gmail.com>
2021-02-22 08:59:50 -0500

    highscore function carries over into new games. basic styling via Bootstrap

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
-// Currently not updating High Score after first one
-// Name = undefined after the first one
+/* incorrect answer penalties are firing upon button creation */
 
 var timer;
 var timerCount;
 var score = 0;
-var highScore = 0;
+var highscore = 0;
+var highScoreDisplay = document.querySelector("#highscore");
 var questionPrompt = document.querySelector("#question");
 var choicesDisplay = document.querySelector("#choices");
 var startButton = document.querySelector("#start");
@@ -90,48 +90,42 @@ function askQuestion() {
         choice.textContent = question.choices[i];
         choice.id = i;
         var id = choice.id;
+        choice.setAttribute("class", "btn-sm btn-primary px-4 mx-2")
         choicesDisplay.appendChild(choice);
     }
     var btns = document.querySelectorAll('button');
     for (var i = 0; i < btns.length; i++) {
-        btns[i].addEventListener('click', function(event) {
-            if (this.id == question.correct) {
-                score++;
-                console.log(this.id);
-                console.log(score);
-                questionNumber++;
-                questionPrompt.removeChild(ask);
-                for ( var i = 0; i < question.choices.length; i++) {
-                    var elem = document.getElementById(i);
-                    elem.parentNode.removeChild(elem);
-                } 
-                askQuestion();
-            } else {
-                // timerCount = (timerCount - 10); // CAUSING PERMANENT 
-                // body.style.opacity = "0.5";
-                // choices.style = "transform: translate(200px, 0); transition-duration: 5s";
-            }
-            
-        })
+        btns[i].addEventListener('click', checkAnswer) 
+    }
+};
+  
+function checkAnswer() {
+    var question = questions[questionNumber];
+    if (this.id == question.correct) {
+        score++;
+        console.log(this.id);
+        console.log(score);
+        questionNumber++;
+        questionPrompt.removeChild(ask);
+        for ( var i = 0; i < question.choices.length; i++) {
+            var elem = document.getElementById(i);
+            elem.parentNode.removeChild(elem);
+        } 
+        askQuestion();
+    } else {
+    // timerCount -= 10;
+    // body.style.opacity = "0.5";
+    // choices.style = "transform: translate(200px, 0); transition-duration: 5s";
     }
 };
 
 function startTimer() {
   // Sets timer
-  console.log(timerCount);
   timer = setInterval(function() {
     timerCount--;
     timerElement.textContent = timerCount;
-    console.log(timerCount);
     // Tests if time has run out
     if (timerCount <= 0) {
-    //   var userName = prompt("What is your name?");
-    //   localStorage.setItem("player", JSON.stringify(userName));
-    //   localStorage.setItem("score", JSON.stringify(score));
-    //   if (score > highScore) {
-    //       highScore = userName + " - " + score;
-    //       document.querySelector("#highscore").textContent = highScore;
-    //   }
       questionPrompt.removeChild(ask);
       while (choicesDisplay.firstChild) {
         choicesDisplay.removeChild(choicesDisplay.lastChild);
@@ -140,7 +134,6 @@ function startTimer() {
       timerCount = 0;
       timerElement.textContent = timerCount;
       gameOver();
-    //   loseGame();
     }
   }, 1000);
 }
@@ -148,20 +141,18 @@ function startTimer() {
 function startGame() {
     console.log(startButton.value);
     startButton.disabled = true;
-    // isWin = false;
     timerCount = 5;
     timerElement.textContent = timerCount;
-    console.log(timerCount);
     startTimer();
     askQuestion();
   };
 
 
   function gameOver() {
-    console.log("WORKING");
+    body.style.opacity = "1";
     var target = document.querySelector('#yourName');
-    
     var form = document.createElement('form');
+    var div = document.createElement('div');
     var label = document.createElement('label');
     var field = document.createElement('input');
     var submit = document.createElement('button');
@@ -174,33 +165,34 @@ function startGame() {
     field.type = "text";
     field.name = "player";
     submit.textContent = "Submit";
-    
+
+    submit.setAttribute("class", "btn-sm btn-success px-4 mx-2");
+    field.setAttribute("class", "form-control");
+    div.setAttribute("class", "form-inline");
     // add elements to DOM
     target.appendChild(form);
-    form.appendChild(label);
-    form.appendChild(field);
+    form.appendChild(div);
+    div.appendChild(label);
+    div.appendChild(field);
     form.appendChild(submit);
-    // submitBtn.addEventListener("click", gameOver);
     submit.addEventListener("click", submitNameLocal);
 };
 
 function submitNameLocal(event) {
     event.preventDefault();
-    console.log("IT IS WORKING");
-    console.log(player.value);
-    player = player.value;
-    localStorage.setItem("player", JSON.stringify(player));
-    localStorage.setItem("score", JSON.stringify(score));
+    var playerName = player.value;
+    console.log(score);
+    console.log(playerName);
+    if (score > highscore) {
+        highscore = score;
+        highScoreDisplay.textContent = playerName + " - " + score;
+    }
     var target = document.querySelector('#yourName');
     var play = document.createElement('button');
     play.id = "play"
     play.textContent = "Play Again?"
     target.appendChild(play);
     play.addEventListener("click", playAgain);
-    if (score > highScore) {
-        highScore = player + " - " + score;
-        document.querySelector("#highscore").textContent = highScore;
-    }
 };
 
 function playAgain() {
@@ -208,7 +200,8 @@ function playAgain() {
     startButton.disabled = false;
     yourName.removeChild(play);
     questionNumber = 0;
-    timerCount = 10;
+    timerCount = 0;
+    score = 0;
     timerElement.textContent = timerCount;
     yourName.removeChild(form);
 };
